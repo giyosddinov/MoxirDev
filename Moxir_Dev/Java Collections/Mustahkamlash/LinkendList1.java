47863c429290d45e9c154f41de1029fe5dbeb6f9
giyosddinovvv <giyosddinovv@gmail.com>
2025-08-06 14:10:04 +0500

    oush

---
+++ Moxir_Dev/Java Collections/Mustahkamlash/LinkendList1.java
@@ -0,0 +1,10 @@
+package Mustahkamlash;
+
+import java.util.LinkedList;
+
+public class LinkendList1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        
+    }
+}
