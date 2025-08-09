eee548c1574e5e7d7dcd327614e8530fc209d4be
giyosddinovvv <giyosddinovv@gmail.com>
2025-08-09 16:52:15 +0500

    push

---
+++ Moxir_Dev/JAVA_COLLECTIONS_MODUL/Map/MapLesson.java
@@ -0,0 +1,17 @@
+public class MapLesson {
+    public static void MapTopshiriq1(){
+        
+    }
+    public static void MapTopshiriq2(){
+
+    }
+    public static void MapTopshiriq3(){
+
+    }
+    public static void MapTopshiriq4(){
+
+    }
+    public static void MapTopshiriq5(){
+
+    }
+}
