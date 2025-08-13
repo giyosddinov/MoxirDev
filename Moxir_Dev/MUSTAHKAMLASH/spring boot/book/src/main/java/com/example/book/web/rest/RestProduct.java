2a080327a26d1228c58a0e760765c5d192d4f091
giyosddinovvv <giyosddinovv@gmail.com>
2025-08-13 19:53:24 +0500

    push

--- Moxir_Dev/MUSTAHKAMLASH/spring boot/book/src/main/java/com/example/book/web/rest/RestProduct.java
+++ Moxir_Dev/MUSTAHKAMLASH/spring boot/book/src/main/java/com/example/book/web/rest/RestProduct.java
@@ -1,11 +1,13 @@
 package com.example.book.web.rest;
 
 import com.example.book.model.Product;
+import com.example.book.model.Student;
+import jakarta.servlet.annotation.WebServlet;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/product")
@@ -39,5 +41,56 @@ public class RestProduct {
         return ResponseEntity.ok(product);
     }
 
-    @PutMapping("/")
+    @PutMapping("/products/{id}")
+    public ResponseEntity update(@PathVariable int id,@RequestBody Product product){
+        product.setName("nimadir");
+        product.setCategory("nimadir");
+        productList.add(product);
+        productList.remove(getFindId(id));
+        return ResponseEntity.ok(productList);
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity findNameProduct(@RequestParam String name){
+        for(Product p:productList){
+            if(p.getName().equalsIgnoreCase(name)){
+                return ResponseEntity.ok(p);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/products/search/{category}")
+    public ResponseEntity findCategoryProduct(@PathVariable String category){
+        boolean tekshir = true;
+        List<Product> pr = new ArrayList<>();
+        for(Product p : productList){
+            if(p.getCategory().equalsIgnoreCase(category)){
+                tekshir = false;
+                pr.add(p);
+            }
+        }
+        if(!tekshir){
+            return ResponseEntity.ok(pr);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/product/sort/prise")
+    public ResponseEntity productSort(){
+        Collections.sort(productList, Comparator.comparing(Product::getPrise));
+        return ResponseEntity.ok(productList);
+    }
+
+    @GetMapping("/product/sts")
+    public ResponseEntity productstatic(){
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Umumuy Maxsulot Soni",productList.size());
+        int umumuy_sum = 0;
+        for(Product p:productList){
+            umumuy_sum+=p.getPrise();
+        }
+        map.put("Umumiy Summa",umumuy_sum);
+        map.put("O'rtacha Narx",umumuy_sum/productList.size());
+        return ResponseEntity.ok(map);
+    }
 }
---
+++ Moxir_Dev/MUSTAHKAMLASH/spring boot/book/src/main/java/com/example/book/web/rest/Valid.java
@@ -0,0 +1,4 @@
+package com.example.book.web.rest;
+
+public @interface Valid {
+}
